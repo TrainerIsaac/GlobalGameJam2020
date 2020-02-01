905e7fe339b3d7d74e979560079f3fa772d17be0
James <Fizzy726@hotmail.com>
2020-02-01 17:55:26 +0000

    RobotSpawning semi working

--- GGJ2020/Assets/Scripts/RobotSpawning.cs
+++ GGJ2020/Assets/Scripts/RobotSpawning.cs
@@ -15,54 +15,89 @@ public class RobotSpawning : MonoBehaviour
     private bool couple2Finished;
     private bool couple3Finished;
 
+    private int currentCouple = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        bool sessionStarted = false;
-        bool couple1Finished = false;
-        bool couple2Finished = false;
-        bool couple3Finished = false;
+        sessionStarted = false;
+        couple1Finished = false;
+        couple2Finished = false;
+        couple3Finished = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnChecker();
-
+        //spawnChecker();
+        //CompletedSession();
     }
 
     void spawnChecker()
     {
         if (sessionStarted == false)
         {
-            //Run the "if x has gone"
             if (couple1Finished == false)
             {
-                sessionStarted = true;
                 Instantiate(firstCouple);
             }
 
             if (couple1Finished == true && couple2Finished == false)
             {
-                sessionStarted = true;
                 Instantiate(secondCouple);
             }
 
             if (couple2Finished == true)
             {
-                sessionStarted = true;
                 Instantiate(thirdCouple);
             }
+            //sessionStarted = true;
         }
     }
 
     void CompletedSession()
     {
-        
+        if (couple1Finished == true)
+        {
+            firstCouple.SetActive(false);
+        }
+
+        if (couple2Finished == true)
+        {
+            secondCouple.SetActive(false);
+        }
+
+        if (couple3Finished == true)
+        {
+            thirdCouple.SetActive(false);
+        }
     }
 
     public void ButtonTesting()
     {
+        spawnChecker();
+        
+        if (currentCouple == 0)
+        {
+            couple1Finished = true;
+        }
 
+        if (currentCouple == 1)
+        {
+            couple2Finished = true;
+        }
+
+        if (currentCouple == 2)
+        {
+            couple3Finished = true;
+        }
+
+        if (currentCouple >= 4)
+        {
+            currentCouple = 0;
+        }
+
+        //CompletedSession();
+        currentCouple++;
     }
 }
